f2765389777bd6fffc06444e7c4ef8085202009a
valentin32k <valentin32k@mail.ru>
2023-06-23 23:30:48 +0300

    feat: Создать структуру проекта и сервер статистики

--- stats-service/client/src/main/java/ru.practicum.statsservice.client/StatsClient.java
+++ stats-service/client/src/main/java/ru.practicum.statsservice.client/StatsClient.java
@@ -2,16 +2,19 @@ package ru.practicum.statsservice.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ru.practicum.statsservice.dto.InputHitDto;
+import ru.practicum.statsservice.dto.OutputHitDto;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StatsClient {
@@ -37,31 +40,34 @@ public class StatsClient {
         return response.body();
     }
 
-    public /*List<OutputHitDto>*/String getStats(Timestamp beginStatTime,
-                                                 Timestamp endStatTime,
-                                                 List<String> uris,
-                                                 boolean isUnique) throws IOException, InterruptedException {
+    public List<OutputHitDto> getStats(Timestamp beginStatTime,
+                                       Timestamp endStatTime,
+                                       List<String> uris,
+                                       boolean isUnique) throws IOException, InterruptedException {
         if (beginStatTime == null || endStatTime == null) {
             return null;
         }
-        String start = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(beginStatTime);
-        String end = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(endStatTime);
-        url += "/stats?";
-        String params = "start=" + start + "&end=" + end;
+        List<String> params = new ArrayList<>();
+        addParam("start", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(beginStatTime), params);
+        addParam("end", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(endStatTime), params);
         if (uris != null && uris.size() > 0) {
-            params += "&uris=" + mapper.writeValueAsString(uris);
+            for (String uri : uris) {
+                addParam("uris", uri, params);
+            }
         }
-        params += "&unique=" + isUnique;
-        System.out.println(url + params);
-        params = URLEncoder.encode(params, StandardCharsets.UTF_8);
-        System.out.println(url + params);
+        addParam("unique", String.valueOf(isUnique), params);
         HttpRequest request = HttpRequest
                 .newBuilder()
                 .GET()
-                .uri(URI.create(url + params))
+                .uri(URI.create(url + "/stats?" + String.join("&", params)))
                 .header("Content-Type", "application/json")
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return response.body();
+        return mapper.readerForListOf(OutputHitDto.class).readValue(response.body());
+    }
+
+    private void addParam(String paramName, String paramValue, List<String> params) throws UnsupportedEncodingException {
+        String charset = Charset.defaultCharset().name();
+        params.add(URLEncoder.encode(paramName, charset) + "=" + URLEncoder.encode(paramValue, charset));
     }
 }
--- stats-service/dto/src/main/java/ru/practicum/statsservice/dto/OutputHitDto.java
+++ stats-service/dto/src/main/java/ru/practicum/statsservice/dto/OutputHitDto.java
@@ -1,8 +1,12 @@
 package ru.practicum.statsservice.dto;
 
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OutputHitDto {
     String app;
     String uri;
